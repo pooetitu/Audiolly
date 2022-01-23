ced30a36925c2205ddd4f1937f013d787e3392e9
pooetitu <pooetitu@gmail.com>
2022-01-23 16:45:04 +0100

    Setup app configuration and default navigation

--- app/src/main/java/com/audiolly/MainActivity.kt
+++ app/src/main/java/com/audiolly/MainActivity.kt
@@ -2,10 +2,20 @@ package com.audiolly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.audiolly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.ic_launcher_background
+            )
+        )
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
