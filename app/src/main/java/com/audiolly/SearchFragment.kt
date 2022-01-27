5feb06bcd1166adb8dda940fc8f441dd7102c9d8
pooetitu <pooetitu@gmail.com>
2022-01-27 19:21:19 +0100

    Add bottom navigation view

---
+++ app/src/main/java/com/audiolly/SearchFragment.kt
@@ -0,0 +1,20 @@
+package com.audiolly
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        parent: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.search_fragment, parent, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
