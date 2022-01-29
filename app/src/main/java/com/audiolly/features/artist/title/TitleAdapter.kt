7201281df6edbcbc2064232f91d8b4c934705ce7
pooetitu <pooetitu@gmail.com>
2022-01-29 01:01:34 +0100

    Add music item for artist page

--- app/src/main/java/com/audiolly/features/artist/title/TitleAdapter.kt
+++ app/src/main/java/com/audiolly/features/artist/title/TitleAdapter.kt
@@ -4,30 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.audiolly.R
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-
 
 class TitleAdapter : RecyclerView.Adapter<TitleItem>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TitleItem {
         return TitleItem(
             LayoutInflater.from(parent.context)
-                .inflate(R.layout.music_ranking_item, parent, false)
+                .inflate(R.layout.music_item, parent, false)
         )
     }
 
     override fun onBindViewHolder(cell: TitleItem, position: Int) {
-        // TODO
-        cell.rank.text = position.toString()
-        cell.songTitle.text = "Gucci Gang"
-        cell.songArtists.text = "Lil Pump"
-        Glide.with(cell.thumbnail.context)
-            .load("https://e.snmc.io/i/600/s/d537cb481e92854ec656a2e68adb13b9/9116225/stylesandcomplete-x-nathaniel-knows-x-purowuan-gucci-gang-stylesandcomplete-x-nathaniel-knows-x-purowuan-remix-Cover-Art.jpg")
-            .centerCrop()
-            .apply(RequestOptions.bitmapTransform(RoundedCorners(10)))
-            .placeholder(R.drawable.ic_placeholder_album)
-            .into(cell.thumbnail)
+        println(position)
+        cell.position.text = position.toString()
+        cell.titleName.text = "Gucci Gang"
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/audiolly/features/artist/title/TitleItem.kt
+++ app/src/main/java/com/audiolly/features/artist/title/TitleItem.kt
@@ -1,14 +1,11 @@
 package com.audiolly.features.artist.title
 
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.music_ranking_item.view.*
+import kotlinx.android.synthetic.main.music_item.view.*
 
 class TitleItem(v: View) : RecyclerView.ViewHolder(v) {
-    val rank: TextView = v.rank
-    val thumbnail: ImageView = v.thumbnail
-    val songTitle: TextView = v.song_title
-    val songArtists: TextView = v.song_artists
+    val position: TextView = v.position
+    val titleName: TextView = v.title_name
 }
